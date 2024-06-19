908d908010f7c984b62790afc1f87bc0e3f943bb
Medhansh Jain <medhanshjain2010@gmail.com>
2024-06-19 12:40:35 +0530

    feat: add approve candidate functionality

---
+++ interviewGPT-fe/src/api/assessmentSheet.tsx
@@ -0,0 +1,11 @@
+import api from "@/components/customAxios/Axios";
+
+export default async function getAssessmentSheet(token: string) {
+  try {
+    const response = await api.get(`/assessment_sheet?token=${token}`);
+    sessionStorage.setItem("question", JSON.stringify(response.data));
+    return response.data;
+  } catch (err: any) {
+    throw err;
+  }
+}
